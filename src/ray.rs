6aa63051caf274129968592a8675e9e0248994d4
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-09-02 16:32:18 -0500

    Ray and Sphere
    Modeling the geometry and some behavior

---
+++ src/ray.rs
@@ -0,0 +1,84 @@
+use super::matrix::Matrix;
+use super::tuple::Tuple;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Ray {
+    pub origin: Tuple,
+    pub direction: Tuple
+}
+
+impl Ray {
+    pub fn new(origin: Tuple, direction: Tuple) -> Self {
+        if !origin.is_point() { panic!("origin should be a point"); }
+        if !direction.is_vector() { panic!("direction should be a vector"); }
+        Ray { origin, direction }
+    }
+
+    pub fn position(&self, t: f64) -> Tuple {
+        self.origin + self.direction * t
+    }
+
+    pub fn transform(&self, m: Matrix) -> Ray {
+        Ray::new(m * self.origin, m * self.direction)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creating_querying_ray() {
+        let origin = Tuple::point(1., 2., 3.);
+        let direction = Tuple::vector(4., 5., 6.);
+        let r = Ray::new(origin, direction);
+
+        assert_eq!(r.origin, origin);
+        assert_eq!(r.direction, direction);
+    }
+
+    #[should_panic]
+    #[test]
+    fn creating_ray_invalid_origin() {
+        let origin = Tuple::vector(1., 2., 3.);
+        let direction = Tuple::vector(4., 5., 6.);
+        Ray::new(origin, direction);
+    }
+
+    #[should_panic]
+    #[test]
+    fn creating_ray_invalid_direction() {
+        let origin = Tuple::point(1., 2., 3.);
+        let direction = Tuple::point(4., 5., 6.);
+        Ray::new(origin, direction);
+    }
+
+    #[test]
+    fn computing_point_from_distance() {
+        let r = Ray::new(Tuple::point(2., 3., 4.), Tuple::vector(1., 0., 0.));
+        assert_eq!(r.position(0.), Tuple::point(2., 3., 4.));
+        assert_eq!(r.position(1.), Tuple::point(3., 3., 4.));
+        assert_eq!(r.position(-1.), Tuple::point(1., 3., 4.));
+        assert_eq!(r.position(2.5), Tuple::point(4.5, 3., 4.));
+    }
+
+    #[test]
+    fn translating_ray() {
+        let r = Ray::new(Tuple::point(1., 2., 3.), Tuple::vector(0., 1., 0.));
+        let m = Matrix::translation(3., 4., 5.);
+        let r2 = r.transform(m);
+
+        assert_eq!(r2.origin, Tuple::point(4., 6., 8.));
+        assert_eq!(r2.direction, Tuple::vector(0., 1., 0.));
+    }
+
+    #[test]
+    fn scaling_ray() {
+        let r = Ray::new(Tuple::point(1., 2., 3.), Tuple::vector(0., 1., 0.));
+        let m = Matrix::scaling(2., 3., 4.);
+        let r2 = r.transform(m);
+
+        assert_eq!(r2.origin, Tuple::point(2., 6., 12.));
+        assert_eq!(r2.direction, Tuple::vector(0., 3., 0.));
+    }
+}
---
+++ src/sphere.rs
@@ -0,0 +1,200 @@
+use super::intersection::{Intersection, Intersections};
+use super::material::Material;
+use super::matrix::{Matrix, IDENTITY_MATRIX};
+use super::ray::Ray;
+use super::shape::{inverse_transform_parameter, Shape, BoxShape};
+use super::tuple::{Tuple, ORIGO};
+use std::any::Any;
+
+#[derive(Debug, Clone)]
+pub struct Sphere {
+    inverse_transform: Matrix,
+    transform: Matrix,
+    material: Material,
+}
+
+impl PartialEq for Sphere {
+    fn eq(&self, other: &Self) -> bool {
+        self.transform == other.transform && self.material == other.material
+    }
+}
+
+impl Default for Sphere {
+    fn default() -> Self {
+        Self {
+            transform: IDENTITY_MATRIX,
+            inverse_transform: IDENTITY_MATRIX,
+            material: Material::default(),
+        }
+    }
+}
+
+impl Shape for Sphere {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn box_eq(&self, other: &dyn Any) -> bool {
+        other.downcast_ref::<Self>().map_or(false, |a| self == a)
+    }
+
+    fn box_clone(&self) -> BoxShape {
+        Box::new((*self).clone())
+    }
+
+    fn inner_intersect(&self, object_ray: Ray) -> Intersections {
+        let sphere_to_ray = object_ray.origin - ORIGO;
+        let a = object_ray.direction.dot(&object_ray.direction);
+        let b = 2.0 * object_ray.direction.dot(&sphere_to_ray);
+        let c = sphere_to_ray.dot(&sphere_to_ray) - 1.;
+        let discriminant = b * b - 4. * a * c;
+
+        if discriminant < 0. {
+            return Intersections::new(vec![]);
+        }
+
+        let i1 = Intersection::new(
+            (-b - discriminant.sqrt()) / (2. * a),
+            Box::new(self.clone()),
+        );
+        let i2 = Intersection::new(
+            (-b + discriminant.sqrt()) / (2. * a),
+            Box::new(self.clone()),
+        );
+        Intersections::new(vec![i2, i1])
+    }
+
+    fn inner_normal_at(&self, object_point: Tuple) -> Tuple {
+        object_point - ORIGO
+    }
+
+    fn material(&self) -> &Material {
+        &self.material
+    }
+
+    fn transformation(&self) -> Matrix {
+        self.transform
+    }
+
+    fn inverse_transformation(&self) -> Matrix {
+        self.inverse_transform
+    }
+}
+
+impl Sphere {
+    pub fn new(material: Option<Material>, transform: Option<Matrix>) -> Self {
+        Self {
+            transform: transform.unwrap_or_default(),
+            inverse_transform: inverse_transform_parameter(transform),
+            material: material.unwrap_or_default(),
+        }
+    }
+
+    pub fn new_boxed(material: Option<Material>, transform: Option<Matrix>) -> BoxShape {
+        Box::new(Sphere::new(material, transform))
+    }
+
+    pub fn default_boxed() -> BoxShape {
+        Box::new(Sphere::default())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ray_intersect_sphere_at_two_points() {
+        let r = Ray::new(Tuple::point(0., 0., -5.), Tuple::vector(0., 0., 1.));
+        let s = Sphere::default();
+        let xs = s.inner_intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 4.);
+        assert_eq!(xs[1].t, 6.);
+    }
+
+    #[test]
+    fn ray_intersect_sphere_at_tangent() {
+        let r = Ray::new(Tuple::point(0., 1., -5.), Tuple::vector(0., 0., 1.));
+        let s = Sphere::default();
+        let xs = s.inner_intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, 5.);
+        assert_eq!(xs[1].t, 5.);
+    }
+
+    #[test]
+    fn ray_miss_sphere() {
+        let r = Ray::new(Tuple::point(0., 2., -5.), Tuple::vector(0., 0., 1.));
+        let s = Sphere::default();
+        let xs = s.inner_intersect(r);
+
+        assert_eq!(xs.len(), 0);
+    }
+
+    #[test]
+    fn ray_originates_insider_sphere() {
+        let r = Ray::new(Tuple::point(0., 0., 0.), Tuple::vector(0., 0., 1.));
+        let s = Sphere::default();
+        let xs = s.inner_intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -1.);
+        assert_eq!(xs[1].t, 1.);
+    }
+
+    #[test]
+    fn sphere_behind_ray() {
+        let r = Ray::new(Tuple::point(0., 0., 5.), Tuple::vector(0., 0., 1.));
+        let s = Sphere::default();
+        let xs = s.inner_intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t, -6.);
+        assert_eq!(xs[1].t, -4.);
+    }
+
+    #[test]
+    fn normal_on_sphere_on_x_axis() {
+        let s = Sphere::default();
+        let n = s.inner_normal_at(Tuple::point(1., 0., 0.));
+
+        assert_eq!(n, Tuple::vector(1., 0., 0.));
+    }
+
+    #[test]
+    fn normal_on_sphere_on_y_axis() {
+        let s = Sphere::default();
+        let n = s.inner_normal_at(Tuple::point(0., 1., 0.));
+
+        assert_eq!(n, Tuple::vector(0., 1., 0.));
+    }
+
+    #[test]
+    fn normal_on_sphere_on_z_axis() {
+        let s = Sphere::default();
+        let n = s.inner_normal_at(Tuple::point(0., 0., 1.));
+
+        assert_eq!(n, Tuple::vector(0., 0., 1.));
+    }
+
+    #[test]
+    fn normal_on_sphere_on_non_axial_point() {
+        let s = Sphere::default();
+        let pv = 3.0f64.sqrt() / 3.0;
+        let n = s.inner_normal_at(Tuple::point(pv, pv, pv));
+
+        assert_eq!(n, Tuple::vector(pv, pv, pv));
+    }
+
+    #[test]
+    fn normal_is_normalized_vector() {
+        let s = Sphere::default();
+        let pv = 3.0f64.sqrt() / 3.0;
+        let n = s.inner_normal_at(Tuple::point(pv, pv, pv));
+
+        assert_eq!(n, n.normalize());
+    }
+}
