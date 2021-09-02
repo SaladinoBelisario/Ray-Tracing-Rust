397f610a9a11065eae8a4e1525691d1adde3bf1e
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-09-02 17:00:25 -0500

    Fixed ORIGO
    ORIGO not found error fixed

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,4 +1,4 @@
-
+use core::ops;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Tuple {
@@ -8,8 +8,132 @@ pub struct Tuple {
     pub w: f64
 }
 
-pub const ORIGIN: Tuple = Tuple { x: 0., y: 0., z: 0., w: 1. };
+pub const ORIGO: Tuple = Tuple { x: 0., y: 0., z: 0., w: 1. };
 pub const VECTOR_Y_UP: Tuple = Tuple { x: 0., y: 1., z: 0., w: 0. };
+
+impl PartialEq for Tuple {
+    fn eq(&self, other: &Self) -> bool {
+        super::approx_eq(self.x, other.x) &&
+            super::approx_eq(self.y, other.y) &&
+            super::approx_eq(self.z, other.z) &&
+            self.w == other.w
+    }
+}
+
+impl ops::Add<Tuple> for Tuple {
+    type Output = Tuple;
+    fn add(self, rhs: Tuple) -> Tuple {
+        Tuple {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+            w: self.w + rhs.w
+        }
+    }
+}
+
+impl ops::Sub<Tuple> for Tuple {
+    type Output = Tuple;
+    fn sub(self, rhs: Tuple) -> Tuple {
+        Tuple {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+            w: self.w - rhs.w
+        }
+    }
+}
+
+impl ops::Neg for Tuple {
+    type Output = Tuple;
+    fn neg(self) -> Tuple {
+        Tuple {
+            x: -self.x ,
+            y: -self.y,
+            z: -self.z,
+            w: -self.w
+        }
+    }
+}
+
+impl ops::Mul<f64> for Tuple {
+    type Output = Tuple;
+    fn mul(self, rhs: f64) -> Tuple {
+        Tuple {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+            w: self.w * rhs
+        }
+    }
+}
+
+impl ops::Div<f64> for Tuple {
+    type Output = Tuple;
+    fn div(self, rhs: f64) -> Tuple {
+        Tuple {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+            w: self.w / rhs
+        }
+    }
+}
+
+impl From<[f64; 4]> for Tuple {
+    fn from(item: [f64; 4]) -> Self {
+        Tuple { x: item[0], y: item[1], z: item[2], w: item[3] }
+    }
+}
+
+impl Tuple {
+    pub fn new(x: f64, y: f64, z: f64, w: f64) -> Self {
+        Self {x, y, z, w}
+    }
+
+    pub fn point(x: f64, y: f64, z: f64) -> Self {
+        Tuple::new(x, y, z, 1.)
+    }
+
+    pub fn vector(x: f64, y: f64, z: f64) -> Self {
+        Tuple::new(x, y, z, 0.)
+    }
+
+    pub fn is_point(&self) -> bool {
+        self.w == 1.
+    }
+
+    pub fn is_vector(&self) -> bool {
+        self.w == 0.
+    }
+
+    pub fn magnitude(&self) -> f64 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    pub fn normalize(&self) -> Tuple {
+        let m = self.magnitude();
+        Tuple::vector(self.x / m, self.y / m, self.z / m)
+    }
+
+    pub fn dot(&self, t: &Tuple) -> f64 {
+        self.x * t.x +
+            self.y * t.y +
+            self.z * t.z +
+            self.w * t.w
+    }
+
+    pub fn cross(&self, t: &Tuple) -> Self {
+        Tuple::vector(
+            self.y * t.z - self.z * t.y,
+            self.z * t.x - self.x * t.z,
+            self.x * t.y - self.y * t.x)
+    }
+
+    pub fn reflect(&self, normal: Tuple) -> Self {
+        *self - normal * 2. * self.dot(&normal)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -40,8 +164,8 @@ mod tests {
     }
 
     #[test]
-    fn origin_is_point() {
-        assert!(ORIGIN.is_point());
+    fn origo_is_point() {
+        assert!(ORIGO.is_point());
     }
 
     #[test]
